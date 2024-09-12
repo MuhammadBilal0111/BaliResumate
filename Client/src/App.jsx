fb071a631a89dea36105b19a202da3a02229c936
Muhammad Bilal <m.bilal0111@gmail.com>
2024-09-12 15:04:06 -0700

    setting up the Routes of the application

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,8 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+
 function App() {
   return (
-    <>
-      <h1 className="bg-red-900 text-4xl">Hello world</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/sign-in" element={<SignIn />}></Route>
+        <Route path="/sign-up" element={<SignUp />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 

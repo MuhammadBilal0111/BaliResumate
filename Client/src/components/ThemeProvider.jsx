6367d6fc849bbf8982ef5783b5caab258bd5acbd
Muhammad Bilal <m.bilal0111@gmail.com>
2024-09-12 16:33:53 -0700

    creating theme provider component

---
+++ Client/src/components/ThemeProvider.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ThemeProvider({ children }) {
+  return (
+    <div className="light">
+      <div className="bg-white text-gray-900 dark:bg-[rgb(4,9,20)] dark:text-gray-200 min-h-screen">
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default ThemeProvider;

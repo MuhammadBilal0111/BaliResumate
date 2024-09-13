23ffe2ed164ae9b1d8e292af286a341312b31f09
Muhammad Bilal <m.bilal0111@gmail.com>
2024-09-12 18:57:45 -0700

    creating Header component UI

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -1,32 +1,32 @@
 import React from "react";
 import { Button, Navbar } from "flowbite-react";
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 function Header() {
+  const path = useLocation().pathname;
   return (
-    <Navbar className="text-center text-4xl shadow-md px-4" fluid rounded>
-      <Navbar.Brand href="" className="flex gap-2">
+    <Navbar className="text-center shadow-md px-4" fluid rounded>
+      <Navbar.Brand href="" className="flex gap-1">
         <img src="/logo.svg" width={40} height={40} alt="" />
         <span className="self-center whitespace-nowrap text-xl font-bold text-orange-400">
           Resume Builder
         </span>
       </Navbar.Brand>
-      <Navbar.Toggle />
       <Navbar.Collapse>
-        <Navbar.Link href="" className="hover:bg-orange-400">
-          Resume Builder App
+        <Navbar.Link active={path === "/resume-build"}>
+          <Link to="/resume-build">Resume builder App</Link>
         </Navbar.Link>
-        <Navbar.Link href="">Resume Templates</Navbar.Link>
-        <Navbar.Link href="">About</Navbar.Link>
-        <Navbar.Link href="">My Account</Navbar.Link>
-      </Navbar.Collapse>
-      <Link to="/sign-in">
-        <Button
-          className="bg-gradient-to-r from-orange-400 to-pink-500"
-          outline
+        <Navbar.Link
+          className="hover:text-red-700"
+          active={path === "/resume-template"}
         >
-          Get started
-        </Button>
-      </Link>
+          <Link to="/resume-templates">Resume Templates</Link>
+        </Navbar.Link>
+        <Navbar.Link active={path === "/about"}>
+          <Link to="/about">About</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
+      <Navbar.Toggle />
     </Navbar>
   );
 }

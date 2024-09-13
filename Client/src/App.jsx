46b959ec6600d2fbd3860b411bde39c65e6d13c5
Muhammad Bilal <m.bilal0111@gmail.com>
2024-09-12 18:56:13 -0700

    creating Header UI

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,10 +3,11 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
-
+import Header from "./components/Header";
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/sign-in" element={<SignIn />}></Route>
--- Client/src/components/ThemeProvider.jsx
+++ Client/src/components/ThemeProvider.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function ThemeProvider({ children }) {
   return (
-    <div className="light">
+    <div className="">
       <div className="bg-white text-gray-900 dark:bg-[rgb(4,9,20)] dark:text-gray-200 min-h-screen">
         {children}
       </div>

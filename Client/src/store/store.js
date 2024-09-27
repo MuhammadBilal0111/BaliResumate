6fdb59e04744cde7d4a32ca11ed2d01b3373aff1
Muhammad Bilal <m.bilal0111@gmail.com>
2024-09-27 00:51:38 -0700

    creating persist state

---
+++ Client/src/store/store.js
@@ -0,0 +1,29 @@
+import userSlice from "./userSlice";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import persistReducer from "redux-persist/es/persistReducer";
+import persistStore from "redux-persist/es/persistStore";
+
+// redux persist is used to store the state of the store in local strorage when the page refresh
+// first make the store and reducer persist
+// first create persist reducer then store
+
+const rootReducer = combineReducers({
+  user: userSlice.reducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware({ serializableCheck: false });
+  },
+});
+export const persistor = persistStore(store);
+export default store;
